8919b02bb1e1a3d29da66308c0f4cb1eded1886b
sora <34794115+skyventuree@users.noreply.github.com>
2022-10-13 15:30:23 +0700

    music changes

--- Sounds/ScreenGameOver music.lua
+++
@@ -1,8 +0,0 @@
-local audio_file = "serenity in ruin.ogg"
-
-local style = ThemePrefs.Get("VisualStyle")
-if style == "Touhou" then
-    audio_file = "sakuramochi.ogg"
-end
-
-return THEME:GetPathS("", audio_file)
